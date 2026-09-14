aab1536cbc4a41fe77de7fb8c198ae91637801dc
oelboss <130381906+oelboss@users.noreply.github.com>
2026-09-14 11:49:00 +0200

    fin de l'addition

--- addition pas fini.py
+++
@@ -1,7 +0,0 @@
-print("saisis un nombre")
-n1=input
-print("ok, un autre")
-n2=input
-print("un deriner...")
-n3=input
-print("la somme des nombres est " +str(n1+n2+n3))
---
+++ addition.py
@@ -0,0 +1,8 @@
+print("saisis un nombre")
+n1=float(input())
+print("ok, un autre")
+n2=float(input())
+print("un dernier...")
+n3=float(input())
+print("la somme des nombres est " +str(n1+n2+n3))
+ 
